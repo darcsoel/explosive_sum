dcee635734696e1c1e894abff93ec3956f0c87e0
Ross <rostikvunarchuk@gmail.com>
2022-06-04 20:26:40 +0300

    two solution for explosive sum issue

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.//
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}...")  # Press Ctrl+F8 to toggle the breakpoint.
+def exp_sum_recursion(number: int, lower_bound=1) -> int:
+    if lower_bound > number:
+        return 0
+    elif lower_bound == number:
+        return 1
+    else:
+        return exp_sum_recursion(number - lower_bound, lower_bound) + exp_sum_recursion(number, lower_bound + 1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
+def exp_sum(number: int) -> int:
+    sum_list = [1 for _ in range(number+1)]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(1, number+1):
+        for j in range(i, number+1):
+            sum_list[j] += sum_list[j-i]
+
+    return sum_list[number] - sum_list[number-1]
--- tests.py
+++ tests.py
@@ -1,1 +1,31 @@
-# import pytest
+from main import exp_sum, exp_sum_recursion
+
+
+def test1():
+    assert exp_sum(1) == 1
+    assert exp_sum_recursion(1) == 1
+
+
+def test2():
+    assert exp_sum(2) == 2  # -> 1+1 , 2
+    assert exp_sum_recursion(2) == 2  # -> 1+1 , 2
+
+
+def test3():
+    assert exp_sum(3) == 3  # -> 1+1+1, 1+2, 3
+    assert exp_sum_recursion(3) == 3  # -> 1+1+1, 1+2, 3
+
+
+def test4():
+    assert exp_sum(4) == 5  # -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+    assert exp_sum_recursion(4) == 5  # -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+
+
+def test5():
+    assert exp_sum(5) == 7  # -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+    assert exp_sum_recursion(5) == 7  # -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+
+
+def test6():
+    assert exp_sum(10) == 42
+    assert exp_sum_recursion(10) == 42
